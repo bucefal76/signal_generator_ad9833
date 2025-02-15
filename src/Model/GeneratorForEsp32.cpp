0b154f5ae9afcabb56d83a909b2047ceac7ec0fe
Bucefal76 <tjakobiec@gmail.com>
2025-02-15 18:41:29 +0100

    ESP works...

--- src/Model/GeneratorForEsp32.cpp
+++ src/Model/GeneratorForEsp32.cpp
@@ -8,18 +8,27 @@
 GeneratorForEsp32::GeneratorForEsp32(const uint8_t spiCsLine)
     : m_spi(nullptr)
 {
+    /*
+        m_spi = new SPIClass(VSPI);
+        m_AD = new AD9833(5, m_spi);
+        m_spi->begin();
 
-    m_AD = new AD9833(spiCsLine, 23, 18);
+        m_AD->begin();
+        m_AD->setSPIspeed(10000000);
+    */
+    /*
+    while (1)
+    {
+
+        m_AD->setFrequency(1000, 0);
+        m_AD->setWave(AD9833_SQUARE1);
+    }
+        */
+
+    m_AD = new AD9833(12, 23, 18);
     if (m_AD != nullptr)
     {
-        Serial.println("created AD");
         m_AD->begin();
-
-        while (1)
-        {
-            m_AD->setFrequency(1000, 0);
-            m_AD->setWave(AD9833_SQUARE1);
-        }
     }
 }
 
