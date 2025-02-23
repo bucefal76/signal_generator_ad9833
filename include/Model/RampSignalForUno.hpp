60008e9da98ac19be3f3185d398939eef4cfcb1b
Bucefal76 <tjakobiec@gmail.com>
2025-02-23 14:02:35 +0100

    DAC object is now member of RampSingalForUno class

--- include/Model/RampSignalForUno.hpp
+++ include/Model/RampSignalForUno.hpp
@@ -9,12 +9,17 @@
     Class controls the ramp signal for wobbulator.
 */
 
+class MCP4725;
+
 class RampSignalForUno : public RampSignalIf
 {
 public:
     RampSignalForUno();
 
     virtual void setValue(const uint16_t signalValue) final;
+
+private:
+    MCP4725 *m_Dac;
 };
 
 #endif
--- src/Model/RampSignalForUno.cpp
+++ src/Model/RampSignalForUno.cpp
@@ -6,37 +6,44 @@
 #include "Wire.h"
 #include "MCP4725.h"
 
-MCP4725 MCP(WOBBULATOR_RAMP_SIGNAL_ADC_I2C_ADDRESS);
-
 RampSignalForUno::RampSignalForUno()
+    : m_Dac(nullptr)
 {
     Wire.begin();
 
-    if (MCP.begin())
-    {
-        MCP.setValue(0U);
-    }
-    else
-    {
-        pinMode(LED_BUILTIN, OUTPUT);
+    m_Dac = new MCP4725(WOBBULATOR_RAMP_SIGNAL_ADC_I2C_ADDRESS);
 
-        while (true)
+    if (m_Dac != nullptr)
+    {
+        if (m_Dac->begin())
         {
+            m_Dac->setValue(0U);
+        }
+        else
+        {
+            pinMode(LED_BUILTIN, OUTPUT);
+
+            while (true)
+            {
 #ifdef USE_SERIAL
-            Serial.println("Cannot communicate with the MCP 4725 DAC!");
+                Serial.println("Cannot communicate with the MCP 4725 DAC!");
 #endif
 
-            digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-            delay(1000);                     // wait for a second
-            digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-            delay(1000);
+                digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+                delay(1000);                     // wait for a second
+                digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+                delay(1000);
+            }
         }
     }
 }
 
 void RampSignalForUno::setValue(const uint16_t signalValue)
 {
-    MCP.setValue(signalValue);
+    if (m_Dac != nullptr)
+    {
+        m_Dac->setValue(signalValue);
+    }
 }
 
 #endif
